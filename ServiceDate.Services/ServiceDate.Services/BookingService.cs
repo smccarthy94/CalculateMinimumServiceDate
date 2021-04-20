9321cd570b2af61dbd0579925a8a5c019284dc5e
Sean McCarthy <sean.mccarthy@lendex.com.au>
2021-04-21 06:34:42 +1000

    empty project structure

---
+++ ServiceDate.Services/ServiceDate.Services/BookingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ServiceDate.Services
+{
+    public interface IBookingService
+    {
+        DateTimeOffset CalculateMinimumServiceDate();
+    }
+
+    public class BookingService : IBookingService
+    {
+        public DateTimeOffset CalculateMinimumServiceDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
