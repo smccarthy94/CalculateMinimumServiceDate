ff71ecac4c1779230227c29ae5225e29a3f82a9c
Sean McCarthy <sean.mccarthy@lendex.com.au>
2021-04-21 06:50:36 +1000

    add core project and holiday service

---
+++ ServiceDate.Services/ServiceDate.Core/NodaTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ServiceDate.Core
+{
+    public static class NodaTimeExtensions
+    {
+        public static DateTimeOffset StartOfDay(this DateTimeOffset date)
+        {
+            return new DateTime(date.Year, date.Month, date.Day, 0, 0, 0, 0);
+        }
+
+        public static bool IsWeekend(this DateTimeOffset date)
+        {
+            if (date.LocalDateTime.DayOfWeek == DayOfWeek.Sunday) return true;
+            if (date.LocalDateTime.DayOfWeek == DayOfWeek.Saturday) return true;
+            return false;
+        }
+    }
+}
---
+++ ServiceDate.Services/ServiceDate.Services/PublicHolidayService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDate.Services
+{
+    public interface IPublicHolidayService
+    {
+        DateTime[] GetPublicHolidays();
+        DateTime NextPublicHoliday(DateTime after);
+        bool IsPublicHoliday(DateTimeOffset date);
+    }
+
+    class PublicHolidayService : IPublicHolidayService
+    {
+        private readonly DateTime[] _publicHolidays = new DateTime[] {
+            new DateTime(2021, 04, 02),
+            new DateTime(2021, 04, 03),
+            new DateTime(2021, 04, 04),
+            new DateTime(2021, 04, 05)
+        };
+
+        DateTime[] GetPublicHolidays() { return _publicHolidays.OrderBy(d => d).ToArray(); }
+        DateTime NextPublicHoliday(DateTime after) { return GetPublicHolidays().FirstOrDefault(d => d > after); }
+
+        bool IsWeekend(DateTimeOffset date)
+        {
+            return new[] { DayOfWeek.Saturday, DayOfWeek.Sunday }.Contains(date.LocalDateTime.DayOfWeek);
+        }
+
+        bool IsPublicHoliday(DateTimeOffset date)
+        {
+            return _publicHolidays.Any(ph => StartOfDay(ph) == StartOfDay(date.LocalDateTime));
+        }
+    }
+}
